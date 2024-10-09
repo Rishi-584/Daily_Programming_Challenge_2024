40ea3fe366fafc2abd3f2d9f887781fffeda0de4
Rushyendra <76088873+Rishi-584@users.noreply.github.com>
2024-10-09 16:20:41 +0530

    Day - 29 - Fibonacci Sequence using Dynamic Programming
    The task is to calculate the n-th Fibonacci number using Dynamic Programming.

---
+++ FibonacciDP.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    dp = [0]*(n+1)
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
+        
+n = 1000
+print(fibonacci(n))
